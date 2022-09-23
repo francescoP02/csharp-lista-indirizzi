c0bd6ef5956e7f6b08bfdf8f3c758ebcc2863447
francescoP02 <file.partipilo@gmail.com>
2022-09-23 16:29:40 +0200

    Added exception for invalid data number and no data insered for Address Obj

---
+++ Address.cs
@@ -0,0 +1,76 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+//Oggi esercitazione veloce, ossia vi chiedo di prendere dimestichezza con quanto appena visto sui file in classe, in particolare nel live-coding di oggi.
+//In questo esercizio dovrete leggere un file CSV, che cambia poco da quanto appena visto nel live-coding in classe, e salvare tutti gli indirizzi contenuti al sul interno all’interno di una lista di oggetti istanziati a partire dalla classe Indirizzo.
+//Attenzione: gli ultimi 3 indirizzi presentano dei possibili “casi particolari” che possono accadere a questo genere di file: vi chiedo di pensarci e di gestire come meglio crediate queste casistiche.
+
+
+
+
+public class Address
+{
+    public string Name { get; set; }
+    public string Surname { get; set; }
+    public string Street { get; set; }
+    public string City { get; set; }
+    public string Province { get; set; }
+    public string Zip { get; set; }
+
+    public Address(string name, string surname, string street, string city, string province, string zip)
+    {
+        if(name == "")
+        {
+            throw new Exception("Name not insered");
+        }
+        else
+        {
+            Name = name;
+        }
+
+        if (surname == "")
+        {
+            throw new Exception("Surname not insered");
+        }
+        else
+        {
+            Surname = surname;
+        }
+
+        if (street == "")
+        {
+            throw new Exception("Street not insered");
+        }
+        else
+        {
+            Street = street;
+        }
+
+        if (city == "")
+        {
+            throw new Exception("City not insered");
+        }
+        else
+        {
+            City = city;
+        }
+
+        if (province == "")
+        {
+            throw new Exception("Province not insered");
+        }
+        else
+        {
+            Province = province;
+        }
+
+        if (zip == "")
+        {
+            throw new Exception("Zip not insered");
+        }
+        else
+        {
+            Zip = zip;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -5,14 +5,80 @@
 //In questo esercizio dovrete leggere un file CSV, che cambia poco da quanto appena visto nel live-coding in classe, e salvare tutti gli indirizzi contenuti al sul interno all’interno di una lista di oggetti istanziati a partire dalla classe Indirizzo.
 //Attenzione: gli ultimi 3 indirizzi presentano dei possibili “casi particolari” che possono accadere a questo genere di file: vi chiedo di pensarci e di gestire come meglio crediate queste casistiche.
 
+using System.Net;
 
-StreamReader file = File.OpenText("C:/.Net/csharp-lista-indirizzi/addresses.csv");
+List<Address> addressesList = new List<Address>();
 
+string path = "C:/.Net/csharp-lista-indirizzi/addresses.csv";
 
-while (!file.EndOfStream)
+if (File.Exists(path))
 {
+    StreamReader file = File.OpenText(path);
 
-    string line = file.ReadLine();
-    Console.WriteLine(line);
+    bool finish = false;
+
+    int count = 0;
+
+    try
+    {
+
+        while (!file.EndOfStream)
+        {
+            if (finish != true) {
+                try
+                {
+                    string row = file.ReadLine();
+
+                    string[] data = row.Split(",");
+
+                    if (data.Count() == 6)
+                    {
+                        addressesList.Add(new Address(data[0], data[1], data[2], data[3], data[4], data[5]));
+
+                        finish = false;
+                    }
+
+                    else
+                    {
+                        Console.WriteLine(Environment.NewLine);
+                        Console.WriteLine($"Error: Invalid data number in line {count + 1}");
+                    }
+
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(Environment.NewLine);
+                    Console.WriteLine($"Error: {e.Message} in line {count + 1}");
+                }
+
+            }
+
+            count++;
+        }
+
+    }
+    catch (IndexOutOfRangeException)
+    {
+        finish = true;
+    }
+
+    finally
+    {
+        file.Close();
+    }
 }
-file.Close();
+
+Console.WriteLine(Environment.NewLine);
+Console.WriteLine("***************Apartment's List***************");
+Console.WriteLine(Environment.NewLine);
+foreach (Address address in addressesList)
+{
+    Console.WriteLine("**********************************************");
+    Console.WriteLine($" Name: {address.Name}");
+    Console.WriteLine($" Surname: {address.Surname}");
+    Console.WriteLine($" City: {address.City}");
+    Console.WriteLine($" Province: {address.Province}");
+    Console.WriteLine($" Street: {address.Street}");
+    Console.WriteLine($" Zip Code: {address.Zip}");
+    Console.WriteLine(Environment.NewLine);
+}
